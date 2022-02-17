265274d79bf3afeb90d7c696fc593297c9615042
koueAnicet <68559596+koueAnicet@users.noreply.github.com>
2022-02-17 17:27:02 +0000

    ajout fonction et images

---
+++ fonctions.py
@@ -0,0 +1,8 @@
+from tkinter import*
+import webbrowser
+
+def beach1():
+    webbrowser.open("https://www.kayak.com/hotels/Villa-Assinie-Bord-de-Lagune,Assinie-Mafia-c103471-h2952994-details/2022-02-18/2022-02-19/2adults?sid=KMFEDHmL0d#overview")
+
+def beach2():
+    webbrowser.open("https://www.kayak.com/hotels/La-Maison-Blanche,Assinie-Mafia-c103471-h3755754-details/2022-02-18/2022-02-19/2adults?sid=KOEEI-fusg#overview")
